cb7b923cbcf5f92e24e2aab009a8c3fa6e0db112
branmar97 <brandonmar97@gmail.com>
2021-04-29 13:33:40 -0400

    add id to listing serializer

--- app/serializers/listing_serializer.rb
+++ app/serializers/listing_serializer.rb
@@ -1,4 +1,4 @@
 class ListingSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :title, :category, :value, :zipcode, :details, :day_price, :created_at, :updated_at, :user
+  attributes :id, :title, :category, :value, :zipcode, :details, :day_price, :created_at, :updated_at, :user
 end
