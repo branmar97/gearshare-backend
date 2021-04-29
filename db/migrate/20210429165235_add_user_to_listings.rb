179d5422ae75c1c051cf95d8c17e8ead4b0447c2
branmar97 <brandonmar97@gmail.com>
2021-04-29 12:53:44 -0400

    add user id to listings

---
+++ db/migrate/20210429165235_add_user_to_listings.rb
@@ -0,0 +1,5 @@
+class AddUserToListings < ActiveRecord::Migration[6.1]
+  def change
+    add_column :listings, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_29_163757) do
+ActiveRecord::Schema.define(version: 2021_04_29_165235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2021_04_29_163757) do
     t.decimal "day_price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
